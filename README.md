82b52675fcc28dc4a81c6f6e76f72bea2fbc788b
vali2wd <vali_nafo@yahoo.com>
2025-11-08 20:21:40 +0200

    Write scripts that support the Read Commited scenario. Update README.md for instructions on which database to use in order for the scenario to work.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Project showcases the different levels of Isolation Levels available in SQL Serv
 
 The database used in the demo is a 2009 Snapshot of Stack Overflow.<br/>
 [Stack Overflow Snapshots through the Years](https://www.brentozar.com/archive/2015/10/how-to-download-the-stack-overflow-database-via-bittorrent/)<br/>
+It is necessary to import such a .bak file into your SQL Server instance to run the demo.<br/>
 
 Credits: [Erik Darling Data](https://www.youtube.com/@ErikDarlingData) <br/>
 Professor: Predut S.
--- Read Commited/Change posts to type 2.sql
+++ Read Commited/Change posts to type 2.sql
@@ -5,4 +5,35 @@
  --| | \ \| |____ / ____ \| |__| | | |___| |__| | |  | | |  | |_| |_   | |  | |____| |__| |
  --|_|  \_\______/_/    \_\_____/   \_____\____/|_|  |_|_|  |_|_____|  |_|  |______|_____/ 
 
- --	Specifies that statements can't read data that was modified but not committed by other transactions. This prevents dirty reads. Data can be changed by other transactions between individual statements within the current transaction, resulting in nonrepeatable reads or phantom data. This option is the SQL Server default.                                                         
+ --	Specifies that statements can't read data that was modified but not committed by other transactions. This prevents dirty reads. Data can be changed by other transactions between individual statements within the current transaction, resulting in nonrepeatable reads or phantom data. This option is the SQL Server default.    
+ 
+BEGIN TRAN;
+	UPDATE p
+	SET p.PostTypeId = 1
+	FROM dbo.Posts p
+	WHERE p.Id IN (
+		927378,
+		927386,
+		927397,
+		3377569);
+	
+	SELECT p.Id, p.PostTypeId
+	FROM dbo.Posts p
+	WHERE p.Id IN (
+		927378,
+		927386,
+		927397,
+		3377569);
+ROLLBACK;
+
+-- IMPORTANT.
+-- Use this to update demo posts to type 1.
+UPDATE p
+SET p.PostTypeId = 2
+FROM dbo.Posts p
+WHERE p.Id IN (
+		927378,
+		927386,
+		927397,
+		3377569
+);
