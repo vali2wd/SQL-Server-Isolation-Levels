5627ed6ba1ddbd5d1b3ebc62cc393d86f6b0f281
vali2wd <vali_nafo@yahoo.com>
2025-11-10 11:54:00 +0200

    Renamed all occurences of committed with uncommitted. This was a learning error, as I later figured out that I was doing the uncommited demo. All theory is fine, in fact. Committed is to come.

--- Read Commited/Change posts to type 1.sql
+++ Read Commited/Change posts to type 1.sql
@@ -1,11 +1,11 @@
- -- _____  ______          _____     _____ ____  __  __ __  __ _____ _______ ______ _____  
- --|  __ \|  ____|   /\   |  __ \   / ____/ __ \|  \/  |  \/  |_   _|__   __|  ____|  __ \ 
- --| |__) | |__     /  \  | |  | | | |   | |  | | \  / | \  / | | |    | |  | |__  | |  | |
- --|  _  /|  __|   / /\ \ | |  | | | |   | |  | | |\/| | |\/| | | |    | |  |  __| | |  | |
- --| | \ \| |____ / ____ \| |__| | | |___| |__| | |  | | |  | |_| |_   | |  | |____| |__| |
- --|_|  \_\______/_/    \_\_____/   \_____\____/|_|  |_|_|  |_|_____|  |_|  |______|_____/ 
+-- ______ _____  ___ ______   _   _ _   _ _____ ________  ______  ________ _____ ___________ 
+-- | ___ \  ___|/ _ \|  _  \ | | | | \ | /  __ \  _  |  \/  ||  \/  |_   _|_   _|  ___|  _  \
+-- | |_/ / |__ / /_\ \ | | | | | | |  \| | /  \/ | | | .  . || .  . | | |   | | | |__ | | | |
+-- |    /|  __||  _  | | | | | | | | . ` | |   | | | | |\/| || |\/| | | |   | | |  __|| | | |
+-- | |\ \| |___| | | | |/ /  | |_| | |\  | \__/\ \_/ / |  | || |  | |_| |_  | | | |___| |/ / 
+-- \_| \_\____/\_| |_/___/    \___/\_| \_/\____/\___/\_|  |_/\_|  |_/\___/  \_/ \____/|___/  
 
- --	Specifies that statements can't read data that was modified but not committed by other transactions. This prevents dirty reads. Data can be changed by other transactions between individual statements within the current transaction, resulting in nonrepeatable reads or phantom data. This option is the SQL Server default.    
+ -- Transactions running at the READ UNCOMMITTED level don't issue shared locks to prevent other transactions from modifying data read by the current transaction. READ UNCOMMITTED transactions are also not blocked by exclusive locks that would prevent the current transaction from reading rows that were modified but not committed by other transactions.     
  
 -- Provoke a dirty read.
 UPDATE p
